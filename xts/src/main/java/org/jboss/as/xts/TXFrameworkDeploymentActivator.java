255851fd6aa12889f512d1a85d27991289de6a49
Paul Robinson <paul.robinson@redhat.com>
2012-02-13 14:56:52 +0000

    Introduce TXFramework into AS7

--- xts/src/main/java/org/jboss/as/xts/TXFrameworkDeploymentActivator.java
+++ xts/src/main/java/org/jboss/as/xts/TXFrameworkDeploymentActivator.java
@@ -2,10 +2,12 @@ package org.jboss.as.xts;
 
 import org.jboss.as.server.DeploymentProcessorTarget;
 import org.jboss.as.server.deployment.Phase;
+import org.jboss.as.xts.txframework.TXFrameworkCDIExtensionProcessor;
 
 public class TXFrameworkDeploymentActivator {
 
     static void activate(final DeploymentProcessorTarget processorTarget) {
         processorTarget.addDeploymentProcessor(Phase.PARSE, Phase.PARSE_TXFRAMEWORK_HANDLERS, new TXFrameworkDeploymentProcessor());
+        processorTarget.addDeploymentProcessor(Phase.POST_MODULE, Phase.POST_MODULE_WELD_PORTABLE_EXTENSIONS + 10, new TXFrameworkCDIExtensionProcessor());
     }
 }
